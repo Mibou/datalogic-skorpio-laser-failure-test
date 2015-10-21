0031f66c976b3cbbbe6fe32311883aa308676edf
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-10-21 04:38:34 +0200

    Initial commit.

---
+++ DatalogicSkorpioLaserTesting/Form.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DatalogicSjorpioLaserTesting
+{
+    partial class FormLaser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // logBox
+            // 
+            this.logBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logBox.Location = new System.Drawing.Point(0, 0);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ReadOnly = true;
+            this.logBox.Size = new System.Drawing.Size(638, 455);
+            this.logBox.TabIndex = 0;
+            // 
+            // FormLaser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(638, 455);
+            this.Controls.Add(this.logBox);
+            this.Name = "FormLaser";
+            this.Text = "Laser bug on Skorpio X3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logBox;
+
+    }
+}
+
---
+++ DatalogicSkorpioLaserTesting/Form.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Datalogic.API;
+
+namespace DatalogicSjorpioLaserTesting
+{
+    public partial class FormLaser : Form
+    {
+        private Datalogic.API.DecodeEvent dcdEvent;
+        private Datalogic.API.DecodeHandle hDcd;
+        private Datalogic.API.DecodeRequest reqType = (Datalogic.API.DecodeRequest)1 | Datalogic.API.DecodeRequest.PostRecurring;
+
+        public FormLaser()
+        {
+            InitializeComponent();
+            hDcd = new DecodeHandle(DecodeDeviceCap.Exists | DecodeDeviceCap.Barcode);
+            hDcd.SoftTrigger(DecodeInputType.Barcode, 60000);
+
+            Log("[BEGIN] Starting laser with 60 sec timeout." + Environment.NewLine);
+
+            dcdEvent = new DecodeEvent(hDcd, reqType, this);
+            dcdEvent.TimeOut += new DecodeTimeOut(dcdEvent_Timeout);
+        }
+        private void dcdEvent_Timeout(object sender, Datalogic.API.DecodeEventArgs e)
+        {
+            hDcd.SoftTrigger(DecodeInputType.Barcode, 60000);
+            Log("[TIMEOUT] Restarting laser" + Environment.NewLine);
+        }
+
+        private void Log(string text)
+        {
+            logBox.Text += text;
+            logBox.Select(logBox.TextLength, 0);
+            logBox.ScrollToCaret();
+        }
+    }
+}
